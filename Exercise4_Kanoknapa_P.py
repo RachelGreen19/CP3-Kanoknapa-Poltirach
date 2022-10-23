d6e349d76d6120e6419073a7573f0904d81be355
RachelGreen19 <116323357+RachelGreen19@users.noreply.github.com>
2022-10-23 11:25:20 +0700

    Create Exercise4_Kanoknapa_P.py

---
+++ Exercise4_Kanoknapa_P.py
@@ -0,0 +1,10 @@
+FEng = 96
+GB = 98.5
+IntroCS = 90
+CP = 92.5
+
+print("----------Your Score-----------")
+print("Foundation English :", FEng)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", IntroCS)
+print("Computer Programming :", CP)
