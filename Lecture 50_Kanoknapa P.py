bb8893757f20eab6e248db618423ccc8e3efd0de
RachelGreen19 <116323357+RachelGreen19@users.noreply.github.com>
2022-10-30 17:09:39 +0700

    Create Lecture 50_Kanoknapa P.py

---
+++ Lecture 50_Kanoknapa P.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(2,6)
+multiplyNumber(9,5)
+minusNumber(9,2)
+divideNumber(79,43)
