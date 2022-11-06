fb76b6aae3176cbb0783164ee5cd3e82cd8426b7
RachelGreen19 <116323357+RachelGreen19@users.noreply.github.com>
2022-11-06 17:41:57 +0700

    Create Lecture 73

--- Lecture 73.py
+++ Lecture 73.py
@@ -1,5 +1,5 @@
+sysmenMenu = {"pork":10,"duck":20,"salmon":30}
 menulist = []
-
 def showBill():
   total = 0
   print("--------------My shop---------------")
@@ -8,14 +8,13 @@ def showBill():
     total = total + menulist[i][1]
   print("Total:",total,"THB")
 
-
 while True:
   menuName = input("Plese Enter Menu :")
   if(menuName.lower() == "exit"):
     break
   else:
-    menuPrice = int(input("Price: "))
-    menulist.append([menuName,menuPrice])
+    menulist.append([menuName,sysmenMenu[menuName]])
 
 print(menulist)
 showBill()
+
